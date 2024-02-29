2db77102fa4978c0344f631e4ecd2fa5bfb0e198
window1 <doserack@gmail.com>
2024-02-29 16:00:34 +0900

    Initial Commit

---
+++ PopupMenu/PopupMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupMenuApp.swift
+//  PopupMenu
+//
+//  Created by window1 on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PopupMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
